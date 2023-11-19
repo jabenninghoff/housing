c06088a97619d08214b8eda7e1f9efb93b198b9c
John Benninghoff <jbenninghoff@mac.com>
2023-11-19 09:36:15 -0600

    Workaround for RStudio markdown issue

---
+++ _dependencies.R
@@ -0,0 +1,4 @@
+# workaround for https://github.com/rstudio/renv/issues/1708 and
+#                https://github.com/rstudio/rstudio/issues/9861
+# TODO: remove _dependencies.R when fixed version of RStudio is released
+library(markdown)
